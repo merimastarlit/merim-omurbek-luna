c39194d65453c7bcd54f96ebe0b361f6edb8c66f
“MEERIM <meerim.us11@gmail.com>
2025-09-25 13:00:48 -0700

    added comments on JS file and also edited id='skills'

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-
+//Dynamic footer with current year
 const footer = document.querySelector("footer");
 const thisYear = new Date().getFullYear();
 const copyright = document.createElement('p');
@@ -7,8 +7,10 @@ footer.appendChild(copyright);
 
 copyright.id = "footer";
 
+
+//Skills section dynamic list
 let skills = ["JavaScript", "Python", "HTML", "CSS", "AWS Cloud", "GCP Cloud", "Git", "GitHub", "DevOps", "ArgoCD", "Kubernetes", "Docker", "GitHub Actions CI", "Helm", "Terraform"];
-let skillsSection = document.getElementById("Skills");
+let skillsSection = document.getElementById("skills");
 let skillsList = skillsSection.querySelector("ul");
 
 for (let i = 0; i < skills.length; i++) {
