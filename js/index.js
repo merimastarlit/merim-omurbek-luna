974720b92e324adcb8e24e4d58a5e8d3a59f17b7
“MEERIM <meerim.us11@gmail.com>
2025-10-07 13:06:40 -0700

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -62,3 +62,62 @@ messageForm.addEventListener("submit", (event) => {
     messageForm.reset();
 
 });
+
+
+
+//Using Fetch API to get GitHub repositories to display in Projects section
+
+// Fetching the repositories from GitHub API
+fetch('https://api.github.com/users/merimastarlit/repos')
+    .then((response) => {
+        //error handling for the fetch request
+        if (!response.ok) {
+            throw new Error('Request failed');
+        }
+        //return the response as JSON
+        return response.json(); // Parse the response as JSON
+    })
+
+    .then((repositories) => {
+
+        console.log(repositories); // Log the repositories to see the data
+
+        // 
+        // Example: display project links
+        const projectSection = document.getElementById("Projects");
+        const projectList = projectSection.querySelector("ul");
+        projectList.classList.add("project-list");
+
+        // Loop through the repositories and create list items with links
+        for (let i = 0; i < repositories.length; i++) {
+            // create a new list item
+            const project = document.createElement("li");
+            const link = document.createElement("a");
+
+            //set the link attributes
+            link.href = repositories[i].html_url;
+
+            //open the link in a new tab
+            link.target = "_blank";
+            link.rel = "noopener noreferrer";
+            //set the link text to the repository name
+            link.textContent = repositories[i].name;
+
+            //filter out forked repositories
+            if (!repositories[i].fork) {
+                //append the link to the list item and the list item to the project list
+                project.appendChild(link);
+                //append the project to the project list
+                projectList.appendChild(project);
+            }
+        }
+    })
+    .catch((error) => {
+        //log any errors that occur during the fetch process
+        console.error('An error occurred:', error);
+        const projectSection = document.getElementById("Projects");
+        const errorMessage = document.createElement("p");
+        errorMessage.textContent = "Sorry, we are unable to load the projects at this time.";
+        projectSection.appendChild(errorMessage);
+    });
+
